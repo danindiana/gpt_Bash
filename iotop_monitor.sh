fc7ac94e0ecb94548ecde3895a87c084a5044f78
danindiana <danindiana@users.noreply.github.com>
2023-11-02 14:14:30 -0500

    Create iotop_monitor.sh
    Copy the script above and save it to a file, e.g., iotop_monitor.sh.
    Make the script executable:
    bash
    Copy code
    chmod +x iotop_monitor.sh
    Run the script as root (since iotop requires root privileges):
    bash
    Copy code
    sudo ./iotop_monitor.sh
    The script will display the iotop output in the terminal and also append it to iotop_monitor.log in the current directory. You can stop the script by pressing Ctrl+C.
    
    The tee command is used to display output in the terminal and simultaneously append it to a file. The -a flag ensures that the output is appended to the file rather than overwriting it.

---
+++ iotop_monitor.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Check if iotop is installed
+if ! command -v iotop &> /dev/null; then
+    echo "iotop is not installed. Please install it first."
+    exit 1
+fi
+
+# Specify the log file in the current directory
+LOGFILE="./iotop_monitor.log"
+
+# Add a header to the log file
+echo "Starting iotop disk monitor on $(date)" | tee -a "$LOGFILE"
+echo "========================================" | tee -a "$LOGFILE"
+
+# Monitor disk I/O activity using iotop
+while true; do
+    echo "Timestamp: $(date)" | tee -a "$LOGFILE"
+    iotop -n 1 -b -o | tee -a "$LOGFILE"  # -n 1: take 1 snapshot, -b: batch mode, -o: only show processes doing I/O
+    echo "----------------------------------------" | tee -a "$LOGFILE"
+    sleep 10  # Wait for 10 seconds before the next snapshot
+done
